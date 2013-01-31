7897a3b8270d5d3a3f9e63a734253fd2b83c928c
Eric Wooley <5wooley4@gmail.com>
2013-01-31 09:04:26 -0800

    Testing out git
    Here we go

--- Resources/app.js
+++ Resources/app.js
@@ -10,6 +10,11 @@
  *  
  */
 
+/*
+ * This is a test to make sure git is working
+ * 
+ */
+
 //bootstrap and check dependencies
 if (Ti.version < 1.8 ) {
 	alert('Sorry - this application template requires Titanium Mobile SDK 1.8 or later');
