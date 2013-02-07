6dd66b2c36594efa3d9c9dbc42bf9b860523a0b0
Eric Wooley <5wooley4@gmail.com>
2013-02-07 15:22:14 -0800

    started writing the api

---
+++ Resources/ui/common/api.js
@@ -0,0 +1,54 @@
+global = require('ui/common/globals');
+
+module.exports.login = function(email, password){
+	
+}
+
+
+function getResponse(url, data){
+	var server = global.httpInterface;
+	var response;
+	server.onload = function()
+	{
+		var json = this.responseText;
+		response = JSON.parse(json);
+	}
+	server.open('POST', url);
+	server.send(data);
+	
+};
+
+
+login = function(email, password)
+{
+	Ti.API.info(email + ' ' + password)
+	var loginReq = module.exports.httpInterface;
+	
+	loginReq.onload = function()
+	{
+		Ti.API.info('Loaded');
+		var json = this.responseText;
+		Ti.API.info(json);
+		var response = JSON.parse(json);
+		if (response.success == true)
+		{
+			var dialog = Ti.UI.createAlertDialog({
+				title: "User Logged In"
+			});
+			dialog.show();
+		}
+		else
+		{
+			var dialog = Ti.UI.createAlertDialog({
+				title: "Email/Password incorrect"
+			});
+			dialog.show();
+		}
+	};
+	loginReq.open("POST","http://winelife.ericwooley.com/login/user_login/");  
+    var params = {  
+        email: email,  
+        password: password  
+    };  
+    loginReq.send(params);
+}
