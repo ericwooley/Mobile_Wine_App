852737d73c468b80d6d13d033ae899b962941993
Eric Wooley <5wooley4@gmail.com>
2013-02-07 09:31:01 -0800

    Modified login form, basic login function

--- Resources/ui/handheld/Login.js
+++ Resources/ui/handheld/Login.js
@@ -8,7 +8,6 @@ function LoginWindow() {
 	});
 	
 	var Body = global.elements.SimpleView('vertical');
-	Body.add(global.elements.SimpleLabel("Login to use this page"));
 	
 	var emailTextField = Ti.UI.createTextField({
 		borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
@@ -21,37 +20,35 @@ function LoginWindow() {
 	var passwordTextField = Ti.UI.createTextField({
 		borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
   		color: '#336699',
-  		width: '80%',
+  		width: '65%',
   		top: 15,
   		hintText: 'Password',
   		passwordMask: true
 	});
-	
-	var showPWtext = Ti.UI.createLabel({
-		text: 'Show Password: ',
-		width: '55%',
-		textAlign: Ti.UI.TEXT_ALIGNMENT_RIGHT
-	})
+
 	var showPWbox = Ti.UI.createSwitch({
     	title:"Show Password",
-    	value:false,
-    	left: 10
+    	value:true,
+    	left: 10,
+    	top: 15
 	});
 	
 	var showPWview = Ti.UI.createView({
-		width: '100%',
+		width: '80%',
 		height: Ti.UI.SIZE,
 		layout: 'horizontal'
 	});
 	
-	showPWview.add(showPWtext);
+	showPWview.add(passwordTextField);
 	showPWview.add(showPWbox);
 	
 	showPWbox.addEventListener('change', function(e){
-		passwordTextField.passwordMask = !e.value;
-		if(Ti.Platform.osname == 'iphone' || Ti.Platform.osname == 'ipad')
-			showPWbox.enabled = false;
-		Ti.API.info(!e.value);
+		passwordTextField.blur();
+		if(!e.value){
+        	passwordTextField.passwordMask = false;
+	    }else{
+	        passwordTextField.passwordMask = true;
+	    }
 	});
 	
 	
@@ -59,44 +56,67 @@ function LoginWindow() {
 	var loginButton = Ti.UI.createButton({
 		title: 'Login',
 		top: 15,
-		width: '80%'
+		width: '80%',
+		enabled: false
 		
 	});
-	var orLabel = Ti.UI.createLabel({
-		text: 'or',
+	
+	var login_Label = Ti.UI.createLabel({
+		text: "Don't have an account? Signing up is easy, just hit register instead of login.",
 		textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
-		top: 10
+		top: 10,
+		width: '80%'
 	});
+	
 	var registerButton = Ti.UI.createButton({
 		title:'Register',
 		top: 10,
-		width: '80%'
+		width: '80%',
+		enabled: false
 	});
 	loginButton.addEventListener('click',function(e)
 	{
 	   Titanium.API.info("User Login Pressed");
 	   
-	   var username = emailTextField.value;
+	   var email = emailTextField.value;
 	   var pw = passwordTextField.value;
 	   
+	   global.login(email, pw);
+	   
+	   /*
 	   self.exitOnClose = false;
 	   self.close();
 	   var ApplicationTabGroup = require('ui/common/ApplicationTabGroup');
 	   new ApplicationTabGroup().open();
+	   */
 	});	
 	
+	function validateFields(e){ 
+		
+		// Email regex
+    	var re = /\S+@\S+\.\S+/;
+		if(re.test(emailTextField.value) && passwordTextField.value.length > 5)
+		{
+			loginButton.enabled = true;
+			registerButton.enabled = true;
+		}
+		else
+		{
+			loginButton.enabled = false;
+			registerButton.enabled = false;
+		}
+	}
+	
+	emailTextField.addEventListener('change', validateFields);
+	passwordTextField.addEventListener('change', validateFields);
 	
 	
 	var loginForm = global.elements.SimpleView('vertical');
-	
-	
-	
 	loginForm.add(emailTextField);
-	loginForm.add(passwordTextField);
-	loginForm.add(loginButton);
-	loginForm.add(orLabel);
-	loginForm.add(registerButton);
 	loginForm.add(showPWview);
+	loginForm.add(loginButton);
+	loginForm.add(registerButton);
+	loginForm.add(login_Label);
 	Body.add(loginForm);
 	self.add(Body);	
 	
