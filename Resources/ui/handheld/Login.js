a0e8d2bd97f9414478008524ff7f50f8345a075b
Eric Wooley <5wooley4@gmail.com>
2013-02-07 14:49:26 -0800

    Updated Login Form

--- Resources/ui/handheld/Login.js
+++ Resources/ui/handheld/Login.js
@@ -8,6 +8,7 @@ function LoginWindow() {
 	});*/
 	
 	var self = global.createWindow("Login");
+	self.navBarHidden = true;
 	
 	var Body = global.elements.SimpleView('vertical');
 	
@@ -22,7 +23,7 @@ function LoginWindow() {
 	var passwordTextField = Ti.UI.createTextField({
 		borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
   		color: '#336699',
-  		width: '65%',
+  		width: '55%',
   		top: 15,
   		hintText: 'Password',
   		passwordMask: true
@@ -30,19 +31,35 @@ function LoginWindow() {
 
 	var showPWbox = Ti.UI.createSwitch({
     	title:"Show Password",
-    	value:true,
     	left: 10,
-    	top: 15
+    	value:true
 	});
 	
 	var showPWview = Ti.UI.createView({
 		width: '80%',
 		height: Ti.UI.SIZE,
-		layout: 'horizontal'
+		layout: 'horizontal',
+	});
+	var pwBoxHint = Ti.UI.createLabel({
+		text: 'Hide Password',
+		font: {fontSize: 14},
+		bottom: 1,
+		color: '#777777'
 	});
 	
+	var pwBoxView = Ti.UI.createView({
+		layout: 'vertical',
+		width: Ti.UI.SIZE,
+		height: Ti.UI.SIZE,
+		top: 15,
+		left: 10
+	});
+	pwBoxView.add(showPWbox);
+	pwBoxView.add(pwBoxHint);
+
+	
 	showPWview.add(passwordTextField);
-	showPWview.add(showPWbox);
+	showPWview.add(pwBoxView);
 	
 	showPWbox.addEventListener('change', function(e){
 		passwordTextField.blur();
@@ -57,7 +74,7 @@ function LoginWindow() {
 	
 	var loginButton = Ti.UI.createButton({
 		title: 'Login',
-		top: 15,
+		top: 5,
 		width: '80%',
 		enabled: false
 		
@@ -66,7 +83,8 @@ function LoginWindow() {
 	var login_Label = Ti.UI.createLabel({
 		text: "Don't have an account? Signing up is easy, just hit register instead of login.",
 		textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
-		top: 10,
+		color: 'black',
+		top: 5,
 		width: '80%'
 	});
 	
@@ -104,22 +122,30 @@ function LoginWindow() {
 	   global.register(email, pw);
 	   
 	});	
+	var fieldHints = Ti.UI.createLabel({
+		text: "To continue, enter a valid E-Mail and 6 character password.",
+		textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
+		width: '80%',
+		top: 5,
+		color: global.colors.dark,
+		font: {fontSize: 12}
+	})
 	function validateFields(e){ 
 		
 		// Email regex
     	var re = /\S+@\S+\.\S+/;
 		if(re.test(emailTextField.value) && passwordTextField.value.length > 5)
 		{
+			fieldHints.hide();
 			loginButton.enabled = true;
 			registerButton.enabled = true;
 		}
 		else
 		{
+			fieldHints.show();
 			loginButton.enabled = false;
 			registerButton.enabled = false;
 		}
-		
-		
 	}
 	
 	emailTextField.addEventListener('change', validateFields);
@@ -127,11 +153,16 @@ function LoginWindow() {
 	
 	
 	var loginForm = global.elements.SimpleView('vertical');
+	
+	loginForm.add(fieldHints);
+	
 	loginForm.add(emailTextField);
 	loginForm.add(showPWview);
+	
 	loginForm.add(loginButton);
-	loginForm.add(registerButton);
 	loginForm.add(login_Label);
+	loginForm.add(registerButton);
+	
 	Body.add(loginForm);
 	self.add(Body);	
 	
