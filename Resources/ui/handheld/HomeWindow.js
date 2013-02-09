4158bf79c7d722f636a36fe484b2b251d3bf4749
Aaron Cheever <allisonjenks@unknown002608de482b.gateway.2wire.net>
2013-02-09 01:35:06 -0800

    Changed whole window into a table

--- Resources/ui/handheld/HomeWindow.js
+++ Resources/ui/handheld/HomeWindow.js
@@ -4,20 +4,49 @@ function HomeWindow(title) {
 		backgroundColor: global.colors.dark
 	});
 	
-	var Head = global.elements.SimpleView();
+	var data = [];
 	
-	var pageTitle = Ti.UI.createLabel({
-		text: 'This is the home page'
+	for (var i = 0; i < 20; i++)
+	{
+		// Record the current row as a string
+		var current_row = 'Wine ' + (i + 1);
+		// Create table tab
+		data[i] = Ti.UI.createTableViewRow({hasChild:true,title:current_row});
+	}
+	
+	// Create a table view
+	var tableview = Titanium.UI.createTableView({
+		data:data
 	});
-	//Head.add(pageTitle);
 	
-	var Body = global.elements.SimpleView();
-	Body.add(pageTitle);
-	Body.add(global.elements.SimpleLabel("This is the Home Content."));
+	function showClickEventInfo(e, islongclick) {
+		// Various event data
+		var index = e.index;
+		var section = e.section;
+		var row = e.row;
+		var rowdata = e.rowData;
+		var msg = 'row ' + row + ', index ' + index + ', section ' + section  + ', row data ' + rowdata;
+		if (islongclick) {
+			msg = "LONGCLICK " + msg;
+		}
+		// Creates popup dialog
+		Titanium.UI.createAlertDialog({title:'Table View',message:msg}).show();
+	}
 	
-	self.add(Body);
+	// Create table view click listener
+	tableview.addEventListener('click', function(e)
+	{
+		showClickEventInfo(e);
+	});
 	
-	//self.add(Head);	
+	// Create a table view longclick listener
+	tableview.addEventListener('longclick', function(e)
+	{
+		showClickEventInfo(e, true);
+	});
+	
+	// Add table view to the window
+	self.add(tableview);
 	
 	return self;
 };
