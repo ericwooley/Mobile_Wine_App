387fe5b3aa47141d74716324a96ffab0a491195b
Matthew Johnson <firefighter85@live.com>
2013-02-12 11:54:35 -0800

    added winereview.js

---
+++ Resources/ui/handheld/WineReview.js
@@ -0,0 +1,13 @@
+
+
+
+function WineReview(){
+ var global = require('ui/common/globals');	
+ var self = global.createWindow('Browse By Color');
+	
+	
+	return self;
+	
+};
+
+module.exports = WineReview;
