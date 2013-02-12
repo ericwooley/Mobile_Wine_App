dcb58fe751e12736c7094a86f71bdb0e0fbbb77a
Matthew Johnson <firefighter85@live.com>
2013-02-12 14:30:51 -0800

    winereview.js update ui

--- Resources/ui/handheld/WineReview.js
+++ Resources/ui/handheld/WineReview.js
@@ -18,7 +18,7 @@ var lbl_WineName = Ti.UI.createLabel({
 	height:'auto'
 
 });
-
+// Label for Company Name
 var lbl_CompanyName = Ti.UI.createLabel({
 	left:'5%',
 	top: '6%',
@@ -28,7 +28,7 @@ var lbl_CompanyName = Ti.UI.createLabel({
 	touchEnabled:false
 
 });		
-
+// Label for Company Location
 var lbl_CompanyLocation = Ti.UI.createLabel({
 	left:'5%',
 	color:global.colors.dark,
@@ -38,10 +38,63 @@ var lbl_CompanyLocation = Ti.UI.createLabel({
 	touchEnabled:false
 
 });	
-	
+// Label for date of review
+var lbl_date = Ti.UI.createLabel({
+	right:5,
+	top:5,
+	color:global.colors.dark,
+	text: "Date",
+	font:{fontSize:12,fontWeight:'normal',fontFamily:'Helvetica Neue'},
+	touchEnabled:false
+});
+
+	// This image will be the image of the wine
+var image = Ti.UI.createImageView({ 		
+  	height: 120,
+  	width: 100,
+  	left: '5%',
+  	top:'20%',
+  	borderColor: global.colors.dark,
+	borderWidth: 1,
+  	contentMode: 'aspectfill',
+  	clipsToBounds: false,
+  	image:'/images/logo.png',
+  	layout:'vertical'
+});
+// Label for Rating
+var lbl_Rating = Ti.UI.createLabel({
+	left: image.right,
+	top:'20%',
+	color:global.colors.dark,
+	text: "Rating: ",
+	font:{fontSize:16,fontWeight:'bold',fontFamily:'Helvetica Neue'},
+	touchEnabled:false
+});
+
+// View to hold description
+var view_description = Ti.UI.createView({
+top:-95,
+left: '40%',
+width: '60%',
+height: (lbl_Rating.bottom - image.bottom)		
+});
+// Label for Description
+	var lbl_Description = Ti.UI.createLabel({
+	left: image.right,
+	color:global.colors.dark,
+	text: "This is where the description goes. i.e. this tastes like grandma or it was like suckling off the teet of jusus.  you get the idea...",
+	font:{fontSize:14,fontWeight:'normal',fontFamily:'Helvetica Neue'},
+	touchEnabled:true
+});
+	view_description.add(lbl_Description);
 	self.add(lbl_WineName);
 	self.add(lbl_CompanyName);
 	self.add(lbl_CompanyLocation);
+	self.add(lbl_date);
+	self.add(image);
+	self.add(lbl_Rating);
+	self.add(lbl_Description);
+	self.add(view_description);
 	return self;
 };
 
