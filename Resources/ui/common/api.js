5164c62297d4db7c9201de825799c81c5dd7e347
Eric Wooley <5wooley4@gmail.com>
2013-02-14 14:07:03 -0800

    profile api functions

--- Resources/ui/common/api.js
+++ Resources/ui/common/api.js
@@ -7,30 +7,23 @@ module.exports.register = function(email, password, callback){
 	getResponse('http://winelife.ericwooley.com/user/register/', {email: email, password:password}, callback);
 }
 
-module.exports.profileInformation = function(){
-	var return_data;
-	getResponse('http://winelife.ericwooley.com/user/profile/', function(data){
-		return_data = data;
-	});
-	alert(return_data);
+module.exports.profileInformation = function(callback){
+	getResponse('http://winelife.ericwooley.com/user/profile/',{}, callback);
 }
 
-module.exports.editProfile = function(){
-	
+module.exports.editProfile = function(fname, lname, bio, callback){
+	getResponse('http://winelife.ericwooley.com/user/update_profile/',{
+		fname: fname,
+		lname: lname,
+		bio: bio
+	}, callback);
 }
 
 module.exports.search_results = require('ui/common/elements/search_results');
 
 module.exports.search = function(query, callback){
 	query = query.replace(' ', '+');
-	getResponse(
-		'http://winelife.ericwooley.com/search/wine_search/',
-		{
-			query: query,
-			size: 1
-		},
-		callback
-	);
+	getResponse( 'http://winelife.ericwooley.com/search/wine_search/', { query: query, size: 1}, callback );
 }
 
 
@@ -44,7 +37,10 @@ module.exports.search = function(query, callback){
 var server = Ti.Network.createHTTPClient();
 module.exports.httpInterface = server;
 function getResponse(url, data, callback){
+	var message = showMessage("please wait: connecting to server", 0);
+	
 	if(Ti.Network.networkType == Ti.Network.NETWORK_NONE){
+		closeMessage(message);
 		var dialog = Ti.UI.createAlertDialog({
 			title: "Connectivity Error",
 			message: "An active internet connection is required"
@@ -52,20 +48,69 @@ function getResponse(url, data, callback){
 		dialog.show();
 		return;
 	}
+	
 	var response;
-	var dia = Ti.UI.createAlertDialog({
-		title: "Please Wait:",
-		message: "connecting to server..."
-	});
-	dia.show();
 	server.onload = function()
 	{
 		var json = this.responseText;
-		response = JSON.parse(json);
-		dia.hide();
+		closeMessage(message);
+		try{
+			response = JSON.parse(json);
+		}catch(err)
+		{
+			showMessage("Decode Error" + err, 5000);
+			response = json;
+		}
+		
 		callback(response);
 	}
+	server.onreadystatechange = function(){
+		for(var info in this){
+			Ti.API.info(info);
+		}
+	}
+	
 	server.open('POST', url);
 	server.send(data);
 	
 };
+
+showMessage = function(customMessage, interval){
+	// window container
+	indWin = Titanium.UI.createWindow();
+	 
+	//  view
+	var indView = Titanium.UI.createView({
+		height:50,
+		width:200,
+		borderRadius:10,
+		backgroundColor:'black',
+		opacity:.9,
+		bottom: 100
+	});
+	indWin.add(indView);
+	 
+	// message
+	var message = Titanium.UI.createLabel({
+	    text: customMessage && typeof(customMessage!=='undefined') ? customMessage : L('please_wait'),
+	    color:'#fff',width:'auto',height:'auto',textAlign:'center',
+	    font:{fontFamily:'Helvetica Neue', fontSize:12,fontWeight:'bold'}
+	});
+	 
+	indView.add(message);
+	indWin.open();
+ 
+	//interval = interval ? interval : 3000;
+	if(interval > 0)
+	{
+		setTimeout(function(){
+			indWin.close({opacity:0,duration:1000});
+		},interval);
+	}
+	return indWin;
+}
+
+closeMessage = function(w)
+{
+	w.close({opacity:0,duration:1000});
+}
--- Resources/ui/common/settings.js
+++ Resources/ui/common/settings.js
@@ -1,3 +1,64 @@
+module.exports = function(w){
+	
+	var firstname = Ti.UI.createTextField({
+		hintText: "First Name",
+		backgroundColor: 'white',
+		top: 10,
+		left: 10,
+		width: 100,
+		style: Ti.UI.INPUT_BORDERSTYLE_BEZEL
+	});
+	var lastname = Ti.UI.createTextField({
+		hintText: "lastName",
+		backgroundColor: 'white',
+		top: 10,
+		left: 10,
+		style: Ti.UI.INPUT_BORDERSTYLE_BEZEL
+	});
+	var bio = Ti.UI.createTextField({
+		hintText: "bio",
+		backgroundColor: 'white',
+		top: 10,
+		left: 10,
+		width: 100,
+		style: Ti.UI.INPUT_BORDERSTYLE_BEZEL
+	 });
+	 var edit = Ti.UI.createButton({
+		title: 'Edit',
+		top: 10,
+		width: 100,
+		height: 50
+	 });
+	 var overview = Ti.UI.createWindow({
+		width: Ti.UI.SIZE,
+		height: Ti.UI.SIZE,
+		top: 10,
+		left: 10,
+		width: "100%",
+		height: Ti.UI.SIZE,
+		layout: 'vertical'
+	});
+	var pi = global.api.profileInformation(function(pi){
+		firstname.value = pi.fname;
+		lastname.value = pi.lname;
+		bio.value = pi.bio;
+		overview.add(firstname);
+		overview.add(lastname);
+		overview.add(bio);
+		overview.add(edit);
+		w.add(overview);
+		edit.addEventListener('click', function(){
+			global.api.editProfile(firstname.value, lastname.value, bio.value, function(result){
+				alert(result);
+			});
+		});
+	});
+}
+
+
+
+
+/* No longer using this, this page is now the testing ground for edit profile.
 // For now this is a demo on how to use the search api.
 
 module.exports = function(w)
@@ -47,3 +108,4 @@ module.exports = function(w)
 	w.add(overview);
 }
 
+*/
