4e854b8ead21d24c15ce6ef0f1ddd463b9510c91
Eric Wooley <5wooley4@gmail.com>
2013-02-16 11:42:27 -0800

    Documented api, added a few functions

--- Resources/ui/common/api.js
+++ Resources/ui/common/api.js
@@ -1,17 +1,45 @@
+/**
+ * Api Documentation
+ * Most of these time, these functions should suffice for interacting with the server.
+ */
 
-module.exports.login = function(email, password, callback){
+var api = {};
+
+/**
+ * @class api
+ * @param {String} the email address to login with
+ * @param {String} The password to login with
+ * @param {Function} The callback function to be called when the request is successful.
+ */
+api.login = function(email, password, callback){
 	getResponse('http://winelife.ericwooley.com/user/login/', {email: email, password:password}, callback);
 }
 
-module.exports.register = function(email, password, callback){
+/**
+ * @class api
+ * @param {String} the email address to register with
+ * @param {String} The password to register with
+ * @param {Function} The callback function to be called when the request is successful.
+ */
+api.register = function(email, password, callback){
 	getResponse('http://winelife.ericwooley.com/user/register/', {email: email, password:password}, callback);
 }
-
-module.exports.profileInformation = function(callback){
+/**
+ * @class api
+ * @param {Function} Callback function, to be called upon success.
+ */
+api.profileInformation = function(callback){
 	getResponse('http://winelife.ericwooley.com/user/profile/', {}, callback);
 }
-
-module.exports.editProfile = function(fname, lname, bio, callback){
+/**
+ * @class api
+ * Send the server an updated profile
+ * @param {String} The users first name
+ * @param {String} The users last name
+ * @param {bio} The users biography text
+ * @param {Function} The callback function to be used 
+ */
+api.editProfile = function(fname, lname, bio, callback){
 	getResponse('http://winelife.ericwooley.com/user/update_profile/',{
 		fname: fname,
 		lname: lname,
@@ -19,21 +47,67 @@ module.exports.editProfile = function(fname, lname, bio, callback){
 	}, callback);
 }
 
-module.exports.search_results = require('ui/common/elements/search_results');
+/**
+ * @class api
+ * creates a view containing a table with all the search results already formatted.
+ * @param {Object} All of the search data. It is easiest to just drop in the results from one of the search functions
+ * @param {Function} Callback function, to be executed when a user clicks on a row.
+ * @return {View} returns a view with all the formatted data.
+ */
+api.search_results = require('ui/common/elements/search_results');
 
-module.exports.search = function(query, callback){
+/**
+ * @class api
+ * general search for items
+ * @param {String} A search query basically words that will be sent to the server.
+ * @param {Function} callback function that will be given the results. It is recommended that you format this data using api.search_results
+ *     @example
+ *     api.search("Some Wine I Want maybe", function(search_results){
+ *         	Ti.API.info('Here are some search results in json format');
+ *          Ti.API.info(JSON.stringify(search_results)); // Check the login for an example of how this object is formatted.
+ *          var preformatteedView = api.search_results(search_results); // Add this view to whatever you want!
+ *     });
+ */
+api.search = function(query, callback){
 	query = query.replace(' ', '+');
 	getResponse( 'http://winelife.ericwooley.com/search/wine_search/', { query: query, size: 10}, callback );
 }
 
 
-
-module.exports.catagory = function(cat, callback){
+/**
+ * @class api
+ * search for a catagories, please note that these catagories need to line up with the wine api catagories. For more information: http://api.wine.com.
+ * @param {String} A search query basically words that will be sent to the server.
+ * @param {Function} callback function that will be given the results. It is recommended that you format this data using api.search_results
+ *     @example
+ *     api.search("124 227 7764", function(search_results){// 124 is red wine, 227 is ? and 7764 is ? I just made them up.
+ *         	Ti.API.info('Here are some search results in json format');
+ *          Ti.API.info(JSON.stringify(search_results)); // Check the login for an example of how this object is formatted.
+ *          var preformatteedView = api.search_results(search_results); // Add this view to whatever you want!
+ *     });
+ */
+api.catagory = function(cat, callback){
 	cat = cat.replace(' ', '+');
 	getResponse( 'http://winelife.ericwooley.com/search/wine_search/', { cat: cat, size: 10}, callback );
 }
 
-
+/**
+ * @class api
+ * General search for a query, with a catagory filter.
+ * @param {String} A search query basically words that will be sent to the server.
+ * @param {Function} callback function that will be given the results. It is recommended that you format this data using api.search_results
+ *     @example
+ *     api.search("124 ", function(search_results){
+ *         	Ti.API.info('Here are some search results in json format');
+ *          Ti.API.info(JSON.stringify(search_results)); // Check the login for an example of how this object is formatted.
+ *          var preformatteedView = api.search_results(search_results); // Add this view to whatever you want!
+ *     });
+ */
+api.search_with_filter = function(query, cat, callback){
+	query = query.replace(' ', '+');
+	cat = cat.replace(' ', '+');
+	getResponse( 'http://winelife.ericwooley.com/search/wine_search/', { query: query, cat: cat, size: 10}, callback );
+}
 
 
 
@@ -50,7 +124,7 @@ var server = Ti.Network.createHTTPClient(
 	     timeout : 5000
 	}
 );
-module.exports.httpInterface = server;
+api.httpInterface = server;
 function getResponse(url, data, callback){
 	var message = showMessage("please wait: connecting to server", 0);
 	
@@ -126,3 +200,5 @@ closeMessage = function(w)
 {
 	w.close({opacity:0,duration:1000});
 }
+
+module.exports = api;
