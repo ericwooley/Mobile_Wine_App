f923ca6419b077477e576e86eb2f235a7d001e30
Eric Wooley <5wooley4@gmail.com>
2013-02-19 13:10:28 -0800

    Checkin page basics

--- Resources/ui/handheld/CheckInsWindow.js
+++ Resources/ui/handheld/CheckInsWindow.js
@@ -13,12 +13,22 @@ function CheckInsWindow(title) {
 	var global = require('ui/common/globals');
 	// Creates the default window with global color scheme
 	var self = global.createWindow(title);
-	
+	var overview = Ti.UI.createView({
+		width: '100%',
+		height: '100%',
+		layout: 'vertical'
+	});
+	var search_view = Titanium.UI.createView({
+		width: 'auto',
+		height: Ti.UI.SIZE,
+		left: 10,
+		right: 10, 
+		layout: 'vertical'
+	});
 	// "Search for wine" text field
-	var wines = Titanium.UI.createTextField({
+	var search_bar = Titanium.UI.createTextField({
 		backgroundColor:'#FFF',
-		top:20,
-		//left:20,
+		top:10,
 		width:'80%',
 		height:40,
 		hintText:'  Search for a wine...',
@@ -29,137 +39,64 @@ function CheckInsWindow(title) {
 		backgroundColor:'transparent'
 	});
 	
-	
-	
-	
-	//window to put results
-	// This is where I am going put all the results.
-	var search_results = global.createWindow(title);
-	
-	
-//******
-	
-	
-	
-	// add my search field to my 
-	search_results.add(wines);
-	
-	// This is the function that fires when someone hides the keyboard.
-	// Maybe a button event or whatever you want.
-	wines.addEventListener('blur', function(){
-		// This is the search api.
-		// The firsty value should be the query followed by an anonymous function
-		// that will be called once the data has been retrieved. This won't be called
-		// if there is a failure. 
-		global.api.search(wines.value, function(result){
-			// get a view with the results.
-			var view = global.api.search_results(result);
-			// Add this view to the view in the whole page.
-			search_results.add(view);
-		});
-	});
-	// add overview to the window.
-	self.add(wines);
-	
-	
-	// NEED SOMETHING HERE *****
-	
-
-//*******
-	
-	
-	
-	
-	
-	// "Year" text field
-	var year = Titanium.UI.createTextField({
-		backgroundColor:'#FFF',
-		bottom:170,
-		left:20,
-		width:'40%',
-		height:40,
-		hintText:'  Year...',
-		paddingLeft:8,
-		paddingRight:8,
-		borderStyle:Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
-		keyboardType:Titanium.UI.KEYBOARD_DEFAULT,
-		backgroundColor:'transparent'
-
-	});
-	
-	// "Style" text field
-	var style = Titanium.UI.createTextField({
-		backgroundColor:'#FFF',
-		bottom:170,
-		right:20,
-		width:'40%',
-		height:40,
-		hintText:'  Style...',
-		paddingLeft:8,
-		paddingRight:8,
-		borderStyle:Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
-		keyboardType:Titanium.UI.KEYBOARD_DEFAULT,
-		backgroundColor:'transparent'
-	});
-	
 	// Creates "Look-Up" button
-	var btn = Titanium.UI.createButton({
-		color:'#000',
+	var search_button = Titanium.UI.createButton({
+		color: 'white',
 		title:"Look-Up",
-		color: global.colors.dark,
-		borderColor: global.colors.dark,
 		borderRadius: 5,
 		borderWidth: 1,
-		backgroundColor: global.colors.lightest,
+		backgroundColor: global.colors.dark,
 		backgroundImage: 'none',
 		font:{fontSize:18,fontWeight:'normal',fontFamily:'Helvetica Neue'},
-		top: 70,
+		top: 10,
 		width:'80%',
-		height:35
+		height:35,
+		zIndex: 1
 		
 	});
 	
-	// Creates "Browse" button
-	var btn2 = Titanium.UI.createButton({
-		color:'#000',
-		title:"Browse",
-		color: global.colors.dark,
-		borderColor: global.colors.dark,
-		borderRadius: 5,
-		borderWidth: 1,
-		backgroundColor: global.colors.lightest,
-		backgroundImage: 'none',
-		font:{fontSize:18,fontWeight:'normal',fontFamily:'Helvetica Neue'},
-		bottom:120,
-		width:'80%',
-		height:35
+	var show_search_button = Ti.UI.createButton(
+		{
+		top: 0,
+		visible: false,
+		title: 'search again',
+		height: 20
+		}
+	);
+	
+	search_view.add(search_bar);
+	search_view.add(search_button);
+	self.add(show_search_button);
+	overview.add(search_view);
+	
+	var results_table = null;
+	search_button.addEventListener('click', function(){
+		search_view.animate({
+			opacity: 0,
+			duration: 1000
+			}, function(d){
+			show_search_button.visible = true;
+			search_view.normalheight = search_view.height;
+			search_view.height = 0;
+		});
+		global.api.search(search_bar.value, function(result){
+			if(results_table)
+				overview.remove(results_table);
+				
+			results_table = global.api.search_results(result);
+			overview.add(results_table);
+		});
 	});
 	
-	// Creates label for "browsing by year/style"
-	var label1 = Titanium.UI.createLabel({
-		color:'#000',            // Black color
-		text:'Browse By Year/Style',
-		font:{fontSize:18,fontFamily:'Helvetica Neue'},
-		textAlign:'center',
-		top: 130,
-		width:'auto'
+	show_search_button.addEventListener('click', function(e){
+		search_view.height = search_view.normalheight;
+		search_view.animate({opacity: 1, duration: 1000}, function(d){
+			show_search_button.visible = false;
+			
+		});
 	});
 	
-	// Displays pop-up message after clicking buttons
-	btn.addEventListener("click", function(e){
-		alert(e.source + " RESULTS FOR SEARCH");
-	});
-	btn2.addEventListener("click", function(e){
-		alert(e.source + " RESULTS FOR BROWSING");
-	});
-	
-	self.add(wines);
-	self.add(year);
-	self.add(style);
-	self.add(btn);
-	self.add(btn2);
-	self.add(label1);
-	//self.open();
+	self.add(overview);
 	global.outputHook(self);
 	return self;
 };
