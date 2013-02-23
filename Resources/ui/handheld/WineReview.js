27fe500af34755d87c77494c03c9cbaed736a580
Eric Wooley <5wooley4@gmail.com>
2013-02-22 20:02:04 -0800

    Cleaning up wine review

--- Resources/ui/handheld/WineReview.js
+++ Resources/ui/handheld/WineReview.js
@@ -7,28 +7,28 @@ function WineReview(wine){
 	var global = require('ui/common/globals');	
 	var self = global.createWindow('Wine Review');
 	var all = wine.all_information;
-	var rating = require('ui/common/elements/RatingControl');
-	
-	// This is the star rating
-	var star_rating = new rating({
-		left: ''
+
+	var overview = Ti.UI.createView({
+		height: '100%',
+		width: '100%',
+		layout: 'vertical'		
 	});
+	Ti.API.info(all.Labels[0].Url);
+	// Add the header to the view.
+	var header = require('ui/handheld/Wine_Review/img_right_text');
+	overview.add(header(
+			wine.name,
+			all.Vineyard.Name,
+			all.Appellation.Name,
+			all.Appellation.Region.Name,
+			all.Labels[0].Url
+		)
+	);
 	
-	// This is the Label for the Wine Name / year
-	var lbl_WineName = Ti.UI.createLabel({
-		left:'5%',
-		color:global.colors.dark,
-		top:'1%',
-		text: wine.name,
-		font:{fontSize:20,fontWeight:'bold',fontFamily:'Helvetica Neue'},
-		touchEnabled:false,
-		height:'auto'
-	});
 	
+	/*
 	// Label for Company Name
 	var lbl_CompanyName = Ti.UI.createLabel({
-		left:'5%',
-		top: '6%',
 		color:global.colors.dark,
 		text: all.Vineyard.Name,
 		font:{fontSize:18,fontWeight:'normal',fontFamily:'Helvetica Neue'},
@@ -37,9 +37,7 @@ function WineReview(wine){
 	
 	// Label for Company Location
 	var lbl_CompanyLocation = Ti.UI.createLabel({
-		left:'5%',
 		color:global.colors.dark,
-		top:'11%',
 		text: all.Appellation.Name + " - " + all.Appellation.Region.Name,
 		font:{fontSize:18,fontWeight:'normal',fontFamily:'Helvetica Neue'},
 		touchEnabled:false
@@ -48,8 +46,6 @@ function WineReview(wine){
 	
 	// Label for type of wine
 	var lbl_type = Ti.UI.createLabel({
-		right:5,
-		top:5,
 		color:global.colors.dark,
 		text: all.Varietal.Name,
 		font:{fontSize:12,fontWeight:'normal',fontFamily:'Helvetica Neue'},
@@ -60,8 +56,6 @@ function WineReview(wine){
 	var image = Ti.UI.createImageView({ 		
 	  	height: 100,
 	  	width: 100,
-	  	left: '5%',
-	  	top:'20%',
 	  	borderRadius: 15,
 	  	borderColor: global.colors.dark,
 		borderWidth: 1,
@@ -74,7 +68,6 @@ function WineReview(wine){
 	
 	// View to hold description
 	var view_description = Ti.UI.createView({
-		top:'20%',
 		left: image.width + 30,
 		width: Ti.UI.SIZE - image.width - 30,
 		height: image.bottom - image.top,
@@ -83,7 +76,6 @@ function WineReview(wine){
 
 	// Label for Price
 	var lbl_Price = Ti.UI.createLabel({
-		left: 0,
 		color:global.colors.dark,
 		text: "$"+all.PriceMin + " - $" + all.PriceMax,
 		font:{fontSize:16,fontWeight:'bold',fontFamily:'Helvetica Neue'},
@@ -94,7 +86,6 @@ function WineReview(wine){
 
 	// Label for Description
 	var lbl_Likes = Ti.UI.createLabel({
-		left: 0,
 		color:global.colors.dark,
 		text: "10 of your friends have liked this wine!",
 		font:{fontSize:14,fontWeight:'normal',fontFamily:'Helvetica Neue'},
@@ -108,28 +99,8 @@ function WineReview(wine){
 	view_description.add(my_rating);
 	
 	// This is the comment button
-	var btn_comment = Ti.UI.createButton({
-		top: '55%',
-		left: '5%',
-		width: '26%',
-		height: '10%',
+	var btn_checkin = Ti.UI.createButton({
 		title:'Comment',
-		color: global.colors.dark,
-		borderColor: global.colors.dark,
-		borderRadius: 5,
-		borderWidth: 1,
-		backgroundColor: global.colors.lightest,
-		backgroundImage: 'none',
-		font:{fontSize:14,fontWeight:'normal',fontFamily:'Helvetica Neue'}
-	});
-	
-	// This is the cheers button
-	var btn_cheers = Ti.UI.createButton({
-		top: '55%',
-		left: '36%',
-		width: '26%',
-		height: '10%',
-		title:'Cheers',
 		color: global.colors.dark,
 		borderColor: global.colors.dark,
 		borderRadius: 5,
@@ -141,10 +112,6 @@ function WineReview(wine){
 	
 	// This is the share button
 	var btn_share = Ti.UI.createButton({
-		top: '55%',
-		right: '5%',
-		width: '26%',
-		height: '10%',
 		title:'Share',
 		color: global.colors.dark,
 		borderColor: global.colors.dark,
@@ -156,15 +123,15 @@ function WineReview(wine){
 	
 	});
 
-	self.add(lbl_WineName);
-	self.add(lbl_CompanyName);
-	self.add(lbl_CompanyLocation);
-	self.add(lbl_type);
-	self.add(image);
-	self.add(view_description);
-	self.add(btn_comment);
-	self.add(btn_share);
-	self.add(btn_cheers);
+	overview.add(lbl_WineName);
+	overview.add(lbl_CompanyName);
+	overview.add(lbl_CompanyLocation);
+	overview.add(lbl_type);
+	overview.add(image);
+	overview.add(view_description);
+	overview.add(btn_checkin);
+	overview.add(btn_share);*/
+	self.add(overview);
 	return self;
 };
 
---
+++ Resources/ui/handheld/Wine_Review/img_right_text.js
@@ -0,0 +1,72 @@
+function header(name, winery, location, region, label_url ){
+	
+	// function that abreviates the state.
+	var st2ab = require('ui/handheld/Wine_Review/abrev_state');
+	// Header overview
+	var h = Ti.UI.createView({
+		layout: 'horizontal',
+		width: '100%',
+		height: Ti.UI.SIZE,
+		top: 10,
+		left: 10,
+		right: 10
+	});
+	// The label image.
+	var bottle_label = Ti.UI.createImageView({ 		
+	  	height: 100,
+	  	width: 100,
+	  	borderRadius: 15,
+	  	borderColor: global.colors.dark,
+		borderWidth: 1,
+	  	contentMode: 'aspectfill',
+	  	clipsToBounds: false,
+	  	image: label_url,
+	});
+	
+	var right_text_view = Ti.UI.createView({
+		layout: 'vertical',
+		width: Ti.UI.FILL,
+		height: Ti.UI.SIZE,
+		left: 10,
+		top: 0
+	});
+	
+	// The text to the right of the image
+	Ti.API.info('name: '+name);
+	var name_lbl = Ti.UI.createLabel({
+		text: name,
+		height: Ti.UI.SIZE,
+		width: Ti.UI.SIZE,
+		left: 0,
+		top: 0
+	});
+	
+	var winery_lbl = Ti.UI.createLabel({
+		text: winery,
+		height: Ti.UI.SIZE,
+		width: Ti.UI.SIZE,
+		left: 0,
+		font:{fontSize:14,fontWeight:'italic',fontFamily:'Helvetica Neue'},
+
+	});
+	
+	var loc_lbl = Ti.UI.createLabel({
+		text: location + ', ' + st2ab(region),
+		height: Ti.UI.SIZE,
+		width: Ti.UI.SIZE,
+		left: 0,
+		font:{fontSize:14,fontWeight:'italic',fontFamily:'Helvetica Neue'},
+	});
+	
+	right_text_view.add(name_lbl);
+	right_text_view.add(winery_lbl);
+	right_text_view.add(loc_lbl);
+	
+	h.add(bottle_label);
+	h.add(right_text_view);
+	
+	return h;
+	
+	
+}
+module.exports = header;
