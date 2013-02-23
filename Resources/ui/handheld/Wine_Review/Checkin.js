a162946a4b3973656a0cd321230b61f751ed6d2f
Eric Wooley <5wooley4@gmail.com>
2013-02-22 23:59:24 -0800

    Fixed some UI problems using TITANUP

--- Resources/ui/handheld/Wine_Review/Checkin.js
+++ Resources/ui/handheld/Wine_Review/Checkin.js
@@ -1,41 +1,45 @@
+
+var TU = require('TitanUp/TitanUp');
 module.exports = function(wine, label_url){
 	var global = require('ui/common/globals');
+	var margin = TU.UI.Sizer.getDimension (10);
+	
+	
+	var overview = Ti.UI.createView({
+		backgroundColor: "#444444",
+		//opacity: .95,
+		top: 10,
+		right: 10,
+		left: 10,
+		height: Ti.UI.SIZE,
+		width: global.android? Ti.UI.FILL: '90%',
+		layout: 'vertical'
+	});
+	
 	if(global.android){
 		var win = Ti.UI.createWindow({title: 'Checkin'});
-		var overview = Ti.UI.createView({
-			backgroundColor: "#444444",
-			layout: 'vertical'
-		});
 	}
 	else
 	{
 		var win = Ti.UI.createWindow({
 			left: 10,
 			right: 10,
-			width: Ti.UI.FILL,
-			height: '80%',
+			width: '100%',
+			height: '100%',
 			title: 'Checkin', 
 
 		});
-		var cancel = Ti.UI.createImageView({
-		  	height: 35,
-		  	width: 35,
-		  	contentMode: 'aspectfill',
-		  	clipsToBounds: false,
-		  	top: 0,
-		  	right: 00,
-		  	image: 'Images/x.png', // this image is released under the GPL, so it needs to be replaced before release, or else our product is released under gpl
-		});	
-		var overview = Ti.UI.createView({
-			backgroundColor: "#444444",
-			//opacity: .95,
+		var cancel = Ti.UI.createButton({
+			title: 'Cancel',
+			height: 25,
 			top: 10,
-			right: 10,
 			left: 10,
-			layout: 'vertical'
-		});	
-		win.add(cancel);
-	}	
+			style: Ti.UI.iPhone.SystemButtonStyle.BORDERED
+		});
+		overview.add(cancel);
+
+	}
+	
 	Ti.API.info(JSON.stringify(wine));
 	var name_lbl = Ti.UI.createLabel({
 		text: wine.Name,
@@ -46,13 +50,13 @@ module.exports = function(wine, label_url){
 		color: 'white'
 	});
 	
-	var rating_wrapper = Ti.UI.createView({
+	/*var rating_wrapper = Ti.UI.createView({
 		height: 50,
 		width: Ti.UI.FILL,
 		backgroundColor: 'green',
 		left: 10, top: 10,
 	})
-	/*var rating = require('ui/common/elements/RatingControl');
+	var rating = require('ui/common/elements/RatingControl');
 	// This is the star rating
 	var star_rating = new rating({left: ''});
 	
@@ -80,12 +84,19 @@ module.exports = function(wine, label_url){
 		right: 10,
 		bottom: 10,
 		borderRadius: 10,
-		backgroundColor: global.colors.dark,
-		color: 'white'
+		//backgroundColor: 'green',
+		//color: 'white'
 	});
-	
+	var rating = TU.UI.createSimplePicker ({
+		left: margin,
+		right: margin,
+		top: margin,
+		title: "Rating: ",
+		values: ['1: I hated it.', '2: I disliked It.', '3: It was mediocre.', '4: I would drink it again.', '5: I LOVED it!']
+	});
 	overview.add(name_lbl);
-	//overview.add(rating_wrapper);
+	overview.add(rating);
+	
 	overview.add(textArea);
 	overview.add(complete_button);
 	win.add(overview);
