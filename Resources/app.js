f94bfc284857819c0b11a1053c966a4c93238f40
Eric Wooley <5wooley4@gmail.com>
2013-02-26 12:13:46 -0800

    Stupid comment to work wth mats github

--- Resources/app.js
+++ Resources/app.js
@@ -1,4 +1,5 @@
- 
+// This is a comment we need to fix
+// HERE WE GO 
 //bootstrap and check dependencies
 if (Ti.version < 1.8 ) {
 	alert('Sorry - this application template requires Titanium Mobile SDK 1.8 or later');
