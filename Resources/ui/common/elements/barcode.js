96a5aadb0dd66e4cd5e5691222cd8434ed6e0619
Ivan Rodriguez <rodrigiv@seawolf.sonoma.edu>
2013-03-09 17:29:50 -0800

    Added code for scanner.
    Added code for scanner in the barcode.js file. Needs to be tested on a
    mobile device.

--- Resources/ui/common/elements/barcode.js
+++ Resources/ui/common/elements/barcode.js
@@ -4,17 +4,84 @@
  * 
  * MEN+1
  * Programmer:  Ivan Rodriguez
- * UNDER CONSTRUCTION
+ * STILL UNDER CONSTRUCTION...
+ * 
+ * COMMENTS: Needs to be tested on a mobile device. Scanner seems to be
+ * functional for iPhone (working to make it work for android). 
+ * When the button for scanning is clicked it does switch to the camera. 
+ * Working on how to switch from camera view back to the "check in" view 
+ * once cancel button gets clicked.
  *************************************************/
 
 module.exports = function(/*result, callback*/){
-	alert('Under Construction');
-	var global = require('ui/common/globals');
 	// Instant overview of product information. Includes prices, most
 	// important retailers, offers, local stores, reviews, etc.
-	var overview = Titanium.UI.createView({
-		// To be filled...
+	
+	var scanditsdk = require("com.mirasense.scanditsdk");
+	
+	// disable the status bar for the camera view on the iphone and android
+	if(Ti.Platform.osname == 'iphone' || Ti.Platform.osname == 'ipad' || Ti.Platform.osname == 'android'){
+		Titanium.UI.iPhone.statusBarHidden = true;
+	}
+	
+	// instantiate the Scandit SDK Barcode Picker view
+	var picker = scanditsdk.createView({
+		"width":Ti.Platform.displayCaps.platformWidth,
+		"height":Ti.Platform.displayCaps.platformHeight
 	});
+	
+	// Initialize the barcode picker.
+	// Parameters: App key "RHAt6oVvEeKMJTSpdzXv4MRLAXBd2XHZQVIWK+HdL8I",
+	//			   The cameraFacingPreference value takes 0 for the back camera.
+	picker.init("RHAt6oVvEeKMJTSpdzXv4MRLAXBd2XHZQVIWK+HdL8I", 0);
+
+	// Set callback functions for when scanning succeedes and for when the 
+	// scanning is canceled.
+	picker.setSuccessCallback(function(e) {
+    alert("success (" + e.symbology + "): " + e.barcode);
+	});
+	picker.setCancelCallback(function(e) {
+    alert("canceled");
+	});
+	
+	// add a tool bar at the bottom of the scan view with a cancel button (iphone/ipad only)
+	picker.showToolBar(true);
+	
+	// Starts the scanning process, and triggers the loading and initialization of the
+	// recognition engine.
+	picker.startScanning();
+	
+	// Adds a search bar to the top of the scan screen.
+	picker.showSearchBar(true);
+	// Sets the text that will be displayed while non-autofocusing cameras are initialized.
+	// It has no effect under android.
+	picker.setTextForInitializingCamera("Initializing camera...");
+	// Sets the text that will be displayed above the viewfinder to tell the user to align
+	// it with the barcode that should be recognized.
+	picker.setTextForInitialScanScreenState("Align code with box");
+	// Enables the recognition of EAN13 and UPC12/UPCA codes.
+	picker.setEan13AndUpc12Enabled(true);
+	// Enables the recognition of EAN8 codes.
+	picker.setEan8Enabled(true);
+	// Enables the recognition of UPCE codes.
+	picker.setUpceEnabled(true);
+	// Enables the recognition of QR codes.
+	picker.setQrEnabled(true);
+	// Enables the sound when a barcode is recognized.
+	picker.setBeepEnabled(true);
+	// Enables the vibration when a barcode is recognized.
+	picker.setVibrateEnabled(true);
+
+	// Create a window to add the picker to and display it. 
+	var window = Titanium.UI.createWindow({  
+    title:'Scandit SDK',
+    backgroundColor:'#fff',
+	});
+	
+	window.add(picker);
+	window.open();
+	
+	return window;
 };
 	
 /*
