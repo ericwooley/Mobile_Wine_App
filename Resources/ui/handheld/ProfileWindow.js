bf2fc5c9eb8c4d0148ef8bf0b263ca7c5ba49e66
David Wells <wellsland@gmail.com>
2013-04-02 11:17:15 -0700

    Profile Page- Add User Image
    Made the user image clickable so that the user can add their own image
    using either their devices built-in camera or a photo from their
    phone's library.

--- Resources/ui/handheld/ProfileWindow.js
+++ Resources/ui/handheld/ProfileWindow.js
@@ -62,6 +62,10 @@ function ProfileWindow(title) {
 
 
 
+
+
+
+
 	//  USER IMAGE - upper left of view
 	//	***********************************************	
 	var user_image = Ti.UI.createImageView({
@@ -77,8 +81,121 @@ function ProfileWindow(title) {
   		//image:'images/user_david.jpg',
   		
 	});
-	header.add(user_image);
 
+
+
+
+
+
+	//  ADD IMAGE
+	//	***********************************************	
+	function add_image(){
+		
+		//Create a dialog with options
+		var dialog = Titanium.UI.createOptionDialog({
+		    //title of dialog
+		    title: 'Choose an image source...',
+		    //options
+		    options: ['Camera','Photo Gallery', 'Cancel'],
+		    //index of cancel button
+		    cancel:2
+		});
+		 
+		//add event listener
+		dialog.addEventListener('click', function(e) {
+			
+		    //if first option was selected
+		    if(e.index == 0)
+		    {
+		        //then we are getting image from camera
+		        Titanium.Media.showCamera({
+		            //we got something
+		            success:function(event)
+		            {
+		                //getting media
+		                var image = event.media; 
+		                user_image.image = image;
+		                 
+		                //checking if it is photo
+		                if(event.mediaType == Ti.Media.MEDIA_TYPE_PHOTO)
+		                {
+		                    //we may create image view with contents from image variable
+		                    //or simply save path to image
+		                    Ti.App.Properties.setString("image", image.nativePath);
+		                }
+		            },
+		            cancel:function()
+		            {
+		                //do somehting if user cancels operation
+		            },
+		            error:function(error)
+		            {
+		                //error happend, create alert
+		                var a = Titanium.UI.createAlertDialog({title:'Camera'});
+		                //set message
+		                if (error.code == Titanium.Media.NO_CAMERA)
+		                {
+		                    a.setMessage('Device does not have camera');
+		                }
+		                else
+		                {
+		                    a.setMessage('Unexpected error: ' + error.code);
+		                }
+		 
+		                // show alert
+		                a.show();
+		            },
+		            allowImageEditing:true,
+		            saveToPhotoGallery:true
+		        });
+		    }
+		    
+		    else if(e.index == 1)
+		    {
+		        //obtain an image from the gallery
+		        Titanium.Media.openPhotoGallery({
+		            success:function(event)
+		            {
+		                //getting media
+		                var image = event.media; 
+		                user_image.image = image;
+		                // set image view
+		                 
+		                //checking if it is photo
+		                if(event.mediaType == Ti.Media.MEDIA_TYPE_PHOTO)
+		                {
+		                    //we may create image view with contents from image variable
+		                    //or simply save path to image
+		                    Ti.App.Properties.setString("image", image.nativePath); 
+		                }   
+		            },
+		            cancel:function()
+		            {
+		                //user cancelled the action fron within
+		                //the photo gallery
+		            }
+		        });
+		    }
+		    else
+		    {
+		        //cancel was tapped
+		        //user opted not to choose a photo
+		    }
+		});
+		 
+		//show dialog
+		dialog.show();
+				
+				
+	}
+	
+
+
+
+
+
+	header.add(user_image);
+	user_image.addEventListener('click', add_image);
 
 
 
