9d57fe29a4e446390ca665c79b204edabf740e46
David Wells <wellsland@gmail.com>
2013-04-09 13:19:55 -0700

    Duplicate refresh button removed on iOS
    Refresh button that is added to the top right of tables.  Added an IF
    statement to only display on android.  This felt unnecessary on iOS
    since we already have a refresh button at the top.

--- Resources/ui/common/elements/search_results.js
+++ Resources/ui/common/elements/search_results.js
@@ -72,6 +72,7 @@ module.exports = function(result, callback){
     	backgroundColor: 'transparent'
 
 	});
+	//  Eric's test refresh button that is added to the top right of tables
 	refresh.add(Ti.UI.createImageView({
 		image: '/images/refresh.png',
 		top: 2,
@@ -84,7 +85,11 @@ module.exports = function(result, callback){
 	})
 	
 	table_view.add(table);
+	
+	//  Added an IF statement to only display on android.  
+	if (Ti.Platform.name === 'Android'){
 	table_view.add(refresh);
+	}
 	
 	return table_view;	
 }
