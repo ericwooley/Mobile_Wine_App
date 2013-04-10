b0b57424ca3f7be51e6a1b909de63bd501e245c3
Eric Wooley <5wooley4@gmail.com>
2013-04-10 11:28:44 -0700

    pull to refresh, doesn't work on discover, that page completion first

--- Resources/ui/common/elements/search_results.js
+++ Resources/ui/common/elements/search_results.js
@@ -83,8 +83,145 @@ module.exports = function(result, callback){
 		table_view.fireEvent('refresh_page_data', {});
 	})
 	
+	//////////////////////////////////
+	// Pull to refresh
+	//////////////////////////////////
+	var border = Ti.UI.createView({
+		backgroundColor:"#576c89",
+		height:2,
+		bottom:0
+	});
+	
+	var tableHeader = Ti.UI.createView({
+		backgroundColor:"#e2e7ed",
+		width:320,
+		height:60
+	});
+	
+	// fake it til ya make it..  create a 2 pixel
+	// bottom border
+	tableHeader.add(border);
+	
+	var arrow = Ti.UI.createView({
+		backgroundImage:"/images/whiteArrow.png",
+		width:23,
+		height:60,
+		bottom:10,
+		left:20
+	});
+	
+	var statusLabel = Ti.UI.createLabel({
+		text:"Pull to reload",
+		left:55,
+		width:200,
+		bottom:30,
+		height:"auto",
+		color:"#576c89",
+		textAlign:"center",
+		font:{fontSize:13,fontWeight:"bold"},
+		shadowColor:"#999",
+		shadowOffset:{x:0,y:1}
+	});
+	
+	// var lastUpdatedLabel = Ti.UI.createLabel({
+		// text:"Last Updated: "+formatDate(),
+		// left:55,
+		// width:200,
+		// bottom:15,
+		// height:"auto",
+		// color:"#576c89",
+		// textAlign:"center",
+		// font:{fontSize:12},
+		// shadowColor:"#999",
+		// shadowOffset:{x:0,y:1}
+	// });
+	
+	var actInd = Titanium.UI.createActivityIndicator({
+		left:20,
+		bottom:13,
+		width:30,
+		height:30
+	});
+
+	tableHeader.add(arrow);
+	tableHeader.add(statusLabel);
+	// tableHeader.add(lastUpdatedLabel);
+	tableHeader.add(actInd);
+	
+	table.headerPullView = tableHeader;
+	
+	var pulling = false;
+	var reloading = false;
+	function beginReloading()
+	{
+		table_view.fireEvent('refresh_page_data', {});
+	}
+	
+	function endReloading()
+	{
+		// simulate loading
+		for (var c=lastRow;c<lastRow+10;c++)
+		{
+			tableView.appendRow({title:"Row "+c});
+		}
+		lastRow += 10;
+	
+		// when you're done, just reset
+		tableView.setContentInsets({top:0},{animated:true});
+		reloading = false;
+		//lastUpdatedLabel.text = "Last Updated: "+formatDate();
+		statusLabel.text = "Pull down to refresh...";
+		actInd.hide();
+		arrow.show();
+	}
+	
+	table.addEventListener('scroll',function(e)
+	{
+		var offset = e.contentOffset.y;
+		if (offset <= -65.0 && !pulling && !reloading)
+		{
+			var t = Ti.UI.create2DMatrix();
+			t = t.rotate(-180);
+			pulling = true;
+			arrow.animate({transform:t,duration:180});
+			statusLabel.text = "Release to refresh...";
+		}
+		else if (pulling && (offset > -65.0 && offset < 0) && !reloading )
+		{
+			pulling = false;
+			var t = Ti.UI.create2DMatrix();
+			arrow.animate({transform:t,duration:180});
+			statusLabel.text = "Pull down to refresh...";
+		}
+	});
+		var event1 = 'dragEnd';
+	if (Ti.version >= '3.0.0') {
+		event1 = 'dragend';
+	}
+
+	table.addEventListener(event1,function(e)
+	{
+		if (pulling && !reloading)
+		{
+			reloading = true;
+			pulling = false;
+			arrow.hide();
+			actInd.show();
+			statusLabel.text = "Reloading...";
+			table.setContentInsets({top:60},{animated:true});
+			arrow.transform=Ti.UI.create2DMatrix();
+			beginReloading();
+		}
+	});
+	
+	
+	
+	////////////////////////////
+	// End of pull to refresh
+	////////////////////////////
 	table_view.add(table);
-	table_view.add(refresh);
+	if(global.android)
+		table_view.add(refresh);
 	
 	return table_view;	
 }
